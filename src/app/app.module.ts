9c551232dca19d254f1b62307ff5df7176c76843
A Venkatesh <avenkateshanand1998@gmail.com>
2024-02-14 16:26:07 +0530

    i added firebase project issue no:#2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,17 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { MaterialModule } from './material/material.module';
-
-
-
+import { environment } from '../environments/environment';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MainComponent } from './main/main.component';
 import { EmployeeComponent } from './main/employee/employee.component';
-
 import { EmployeService } from './shared/employe.service';
-
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { AngularFireModule} from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 
 @NgModule({
   declarations: [
@@ -28,13 +25,13 @@ import { ReactiveFormsModule } from '@angular/forms';
     MaterialModule,
     CommonModule,
     ReactiveFormsModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig)
   ],
   providers: [
-    provideClientHydration(),
     provideAnimationsAsync(),
     EmployeService,
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
-
+export class AppModule {}
